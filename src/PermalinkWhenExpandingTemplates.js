ea9aa0ccc86e26093ac469582ba13c58728110c2
Helder <he7d3r@gmail.com>
2014-09-27 20:49:02 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/PermalinkWhenExpandingTemplates.js
+++ src/PermalinkWhenExpandingTemplates.js
@@ -3,34 +3,34 @@
  * @author: Helder (https://github.com/he7d3r)
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  */
-( function ( $, mw /* , undefined */ ) {
-'use strict';
+( function ( mw, $ ) {
+	'use strict';
 
-var addPermaLink = function () {
-	var param = {
-		'wpInput': $('#wpInput').val(),
-		'wpContextTitle': $.trim( $('#contexttitle').val() )
+	var addPermaLink = function () {
+		var param = {
+			'wpInput': $('#wpInput').val(),
+			'wpContextTitle': $.trim( $('#contexttitle').val() )
+		};
+		if ($('#removecomments').is(':checked')) {
+			param.wpRemoveComments = true;
+		}
+		if ($('#removenowiki').is(':checked')) {
+			param.wpRemoveNowiki = true;
+		}
+		if ($('#generate_xml').is(':checked')) {
+			param.wpGenerateXml = true;
+		}
+		if ($('#generate_rawhtml').is(':checked')) {
+			param.wpGenerateRawHtml = true;
+		}
+		$('legend').append(' (<a href="' +
+			mw.util.getUrl('Special:ExpandTemplates') +
+			'?' + $.param(param) +
+			'">link permanente para o teste atual</a>)');
 	};
-	if ($('#removecomments').is(':checked')) {
-		param.wpRemoveComments = true;
-	}
-	if ($('#removenowiki').is(':checked')) {
-		param.wpRemoveNowiki = true;
-	}
-	if ($('#generate_xml').is(':checked')) {
-		param.wpGenerateXml = true;
-	}
-	if ($('#generate_rawhtml').is(':checked')) {
-		param.wpGenerateRawHtml = true;
-	}
-	$('legend').append(' (<a href="' +
-		mw.util.getUrl('Special:ExpandTemplates') +
-		'?' + $.param(param) +
-		'">link permanente para o teste atual</a>)');
-};
 
-if (mw.config.get('wgCanonicalSpecialPageName') === 'ExpandTemplates') {
-	$(addPermaLink);
-}
+	if (mw.config.get('wgCanonicalSpecialPageName') === 'ExpandTemplates') {
+		$(addPermaLink);
+	}
 
-}( jQuery, mediaWiki ) );
+}( mediaWiki, jQuery ) );
