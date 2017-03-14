17d6d3e20b2a398eb2a17a20284a11cb0f178cff
Helder <he7d3r@gmail.com>
2017-03-14 09:44:07 -0300

    Apply coding conventions (jscs)

--- src/PermalinkWhenExpandingTemplates.js
+++ src/PermalinkWhenExpandingTemplates.js
@@ -1,5 +1,6 @@
 /**
  * Add a permanent link to [[Special:ExpandTemplates]]
+ *
  * @author: Helder (https://github.com/he7d3r)
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  */
@@ -8,29 +9,29 @@
 
 	var addPermaLink = function () {
 		var param = {
-			'wpInput': $('#input textarea').val(),
-			'wpContextTitle': $.trim( $('#contexttitle input').val() )
+			wpInput: $( '#input textarea' ).val(),
+			wpContextTitle: $.trim( $( '#contexttitle input' ).val() )
 		};
-		if ($('#removecomments input').is(':checked')) {
+		if ( $( '#removecomments input' ).is( ':checked' ) ) {
 			param.wpRemoveComments = true;
 		}
-		if ($('#removenowiki input').is(':checked')) {
+		if ( $( '#removenowiki input' ).is( ':checked' ) ) {
 			param.wpRemoveNowiki = true;
 		}
-		if ($('#generate_xml input').is(':checked')) {
+		if ( $( '#generate_xml input' ).is( ':checked' ) ) {
 			param.wpGenerateXml = true;
 		}
-		if ($('#generate_rawhtml input').is(':checked')) {
+		if ( $( '#generate_rawhtml input' ).is( ':checked' ) ) {
 			param.wpGenerateRawHtml = true;
 		}
-		$('#ooui-0').before(' (<a href="' +
-			mw.util.getUrl('Special:ExpandTemplates') +
-			'?' + $.param(param) +
-			'">link permanente para o teste atual</a>)');
+		$( '#ooui-0' ).before( ' (<a href="' +
+			mw.util.getUrl( 'Special:ExpandTemplates' ) +
+			'?' + $.param( param ) +
+			'">link permanente para o teste atual</a>)' );
 	};
 
-	if (mw.config.get('wgCanonicalSpecialPageName') === 'ExpandTemplates') {
-		$(addPermaLink);
+	if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'ExpandTemplates' ) {
+		$( addPermaLink );
 	}
 
 }( mediaWiki, jQuery ) );
